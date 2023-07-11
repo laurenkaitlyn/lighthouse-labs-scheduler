7ddcca5f8550c856ec2ad65fa47f2faca5b279c8
Lauren <laurenkaitlyn@gmail.com>
2023-07-11 12:59:35 -0400

    implement getInterviewersForDay

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,7 @@
 import React from "react";
 import DayList from "components/DayList";
 import Appointment from "./Appointment/index";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -20,21 +20,27 @@ export default function Application(props) {
   useEffect(() => {
     const daysUrl = `http://localhost:8001/api/days`;
     const appointmentsURL = `http://localhost:8001/api/appointments`;
+    const interviewers = `http://localhost:8001/api/interviewers`;
 
-    Promise.all([axios.get(daysUrl), axios.get(appointmentsURL)]).then(
-      (all) => {
-        console.log(all[0]);
-        console.log(all[1]);
-        setState((prev) => ({
-          ...prev,
-          days: all[0].data,
-          appointments: all[1].data,
-        }));
-      }
-    );
+    Promise.all([
+      axios.get(daysUrl),
+      axios.get(appointmentsURL),
+      axios.get(interviewers),
+    ]).then((all) => {
+      console.log(all[0]);
+      console.log(all[1]);
+      console.log(all[2]);
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -57,8 +63,14 @@ export default function Application(props) {
       <section className="schedule">
         {dailyAppointments.map((appointment) => {
           const interview = getInterview(state, dailyAppointments.interview);
-
-          return <Appointment key={appointment.id} {...appointment} />;
+          return (
+            <Appointment
+              key={appointment.id}
+              id={appointment.id}
+              time={appointment.time}
+              interview={appointment.interview}
+            />
+          );
         })}
         <Appointment key="last" time="5pm" />
       </section>

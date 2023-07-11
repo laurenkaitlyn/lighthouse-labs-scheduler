8197e90d4ea6a11bee11ef362f19d0c3bc913bb2
Lauren <laurenkaitlyn@gmail.com>
2023-07-11 12:56:46 -0400

    add interview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,15 @@
 // returns an array of appointments for the given day.
 export function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter(d => d.name === day)
+  const filteredDays = state.days.filter((d) => d.name === day);
 
   if (state.days.length === 0 || filteredDays.length === 0) {
-    return []
-  };
+    return [];
+  }
 
-  const appointmentsFromDays = filteredDays[0].appointments;
+  const appointmentsForDays = filteredDays[0].appointments;
   let filteredAppointments = [];
 
-  for(let appointment of appointmentsFromDays) {
+  for (let appointment of appointmentsForDays) {
     filteredAppointments.push(state.appointments[appointment]);
   }
   return filteredAppointments;
@@ -26,4 +26,23 @@ export function getInterview(state, interview) {
   filteredInterviews.interviewer = state.interviewers[interview.interviewer];
 
   return filteredInterviews;
+}
+
+//takes a state object and a day string as arguments and returns an array of interviewers for the specified day from the state object.
+export function getInterviewersForDay(state, day) {
+  const interviewersForDay = [];
+
+  state.days.forEach((d) => {
+    if (d.name === day) {
+      d.appointments.forEach((interviewerId) => {
+        interviewersForDay.push(state.appointments[interviewerId]);
+      });
+    }
+  });
+  /*
+  checks if the interviewersForDay array has any elements
+  if it does, it returns the array containing the interviewers for the specified day
+  otherwise, it returns an empty array
+  */
+  return interviewersForDay.length ? interviewersForDay : [];
 }

f64907fb699871f10d08abc19eb7bb73ac2b20fd
Lauren <laurenkaitlyn@gmail.com>
2023-08-05 16:13:10 -0400

    update form validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,6 +14,11 @@ export default function Form(props) {
       return;
     }
 
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
     setError("");
     props.onSave(student, interviewer);
   }

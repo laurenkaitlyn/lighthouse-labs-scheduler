26c8ae9c6a5d083bb9f78fbbb1773b940a628465
Lauren <laurenkaitlyn@gmail.com>
2023-09-12 11:27:26 -0400

    fixed setHistory state change

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -16,7 +16,7 @@ export function useVisualMode(initial) {
 
   function back() {
     if (history.length > 1) {
-      setHistory(history.slice(0, -1));
+      setHistory((prev) => prev.slice(0, -1));
       setMode(history[history.length - 2]);
     }
   }
